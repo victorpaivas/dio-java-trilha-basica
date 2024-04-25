386d54396b6b07bd4edd8f9a7d2fffda2156cb58
Victor Paiva <129319485+victorpaivas@users.noreply.github.com>
2024-04-25 12:39:53 -0300

    Desafio banco DIO trilha Java básico.

--- src/Conta.java
+++ src/Conta.java
@@ -3,7 +3,27 @@ public class Conta {
     int numero;
     double saldo;
     String agencia;
-    String cliente;
+    String nomeCliente;
+    String sobrenomeCliente;
 
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public void setSobrenomeCliente(String sobrenomeCliente) {
+        this.sobrenomeCliente = sobrenomeCliente;
+    }
 }
 
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,32 +1,38 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
         // Instânciação dos objetos do tipo Conta
         Conta conta = new Conta();
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.ENGLISH);
 
         //Mensagem de boas vindas
         System.out.println("Olá bem vindo ao seu banco digital!");
         System.out.println("Por favor, abaixo iremos pedir algumas informações para abertura da conta.");
 
         //Entrada de dados
-        System.out.print("Por favor digite o número da conta: ");
-        conta.numero = scanner.nextInt();
+        System.out.print("Digite seu nome: ");
+        conta.nomeCliente = input.next();
 
-        System.out.print("Digite o número da agência: ");
-        conta.agencia = scanner.next();
+        System.out.print("Digite seu sobrenome: ");
+        conta.sobrenomeCliente = input.next();
 
-        System.out.print("Digite o saldo a ser depositado inicialmente: ");
-        conta.saldo = scanner.nextDouble();
+        System.out.print("Digite o número da sua conta: ");
+        conta.numero = input.nextInt();
 
-        System.out.print("Digite o nome completo do titular da conta: ");
-        conta.cliente = scanner.nextLine();
+        System.out.print("Digite sua agência: ");
+        conta.agencia = input.next();
+
+        System.out.print("Digite o saldo a ser depositado: ");
+        conta.saldo = input.nextDouble();
 
 
         // Saída dos dados
-        System.out.println("Olá " + conta.cliente + ", obrigado por criar uma conta em nosso banco, " +
-                "sua agência é " + conta.agencia + " conta " + conta.numero + " e seu saldo R$ " + conta.saldo +
+        System.out.println("Olá " + conta.nomeCliente + " " + conta.sobrenomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + conta.agencia + " conta " + conta.numero);
+        System.out.println(" e seu saldo R$" + conta.saldo +
                 " já está disponível para saque.");
 
     }
